fbd030277f33af7ccddb79f5546f6f9c4a3b7e71
andrew webber (personal) <andrewvwebber@googlemail.com>
2017-01-28 23:35:53 +0100

    initial pull

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 go install github.com/andrewwebber/kate
-go install github.com/coreos/clair/contrib/analyze-local-images
+go install github.com/andrewwebber/kate/contrib/analyze-local-images
 cp ./bin/kate ./src/github.com/andrewwebber/kate/container
 cp ./bin/analyze-local-images ./src/github.com/andrewwebber/kate/container
-docker build -t andrewwebber/kate ./src/github.com/andrewwebber/kate/container
-docker push andrewwebber/kate
+docker build -t andrewwebber/kate:next ./src/github.com/andrewwebber/kate/container
+docker push andrewwebber/kate:next
 rm ./src/github.com/andrewwebber/kate/container/kate
 rm ./src/github.com/andrewwebber/kate/container/analyze-local-images
